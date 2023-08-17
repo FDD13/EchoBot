c3c13a2cf6bf31385b26869952f892de5e91e9f5
FDD13 <122601072+FDD13@users.noreply.github.com>
2023-08-17 12:49:01 +0000

    Урок 7. Считаем секунды в Telegram

--- main.py
+++ main.py
@@ -4,22 +4,21 @@ import random
 from pytimeparse import parse
 
 
-first_secret = os.environ['TOKEN']
-second_secret = os.environ['TG_CHAT_ID']
-bot = ptbot.Bot(first_secret)
-bot.send_message(second_secret, "Привет!")
-bot.send_message(second_secret, "Запускаю таймер...")
+FIRST_SECRET = os.environ['TOKEN']
+SECOND_SECRET = os.environ['TG_CHAT_ID']
+BOT = ptbot.Bot(FIRST_SECRET)
+
 
 
 def notify_progress(secs_left, second_secret, 
                     message_id, msg):
     progressbar = render_progressbar(msg, secs_left)
-    bot.update_message(second_secret, message_id, 
+    BOT.update_message(second_secret, message_id, 
                        "Осталось {} секунд\n{}".format(secs_left, progressbar))
     
 
 def notify():
-    bot.send_message(second_secret, "Время вышло!")
+    BOT.send_message(SECOND_SECRET, "Время вышло!")
 
 
 def render_progressbar(msg, secs_left, 
@@ -36,16 +35,19 @@ def render_progressbar(msg, secs_left,
 
 def choose(second_secret, question):
     msg = parse(question)
-    message_id = bot.send_message(second_secret, "Запускаю таймер...")
-    bot.create_countdown(msg, notify_progress,
+    message_id = BOT.send_message(second_secret, "Запускаю таймер...")
+    BOT.create_countdown(msg, notify_progress,
                          second_secret=second_secret,
-                         message_id=message_id, msg=msg)
-    bot.create_timer(msg, notify)
+                         message_id=message_id, 
+                         msg=msg)
+    BOT.create_timer(msg, notify)
 
 
 def main():
-    bot.reply_on_message(choose)
-    bot.run_bot() 
+    BOT.send_message(SECOND_SECRET, "Привет!")
+    BOT.send_message(SECOND_SECRET, "Запускаю таймер...")
+    BOT.reply_on_message(choose)
+    BOT.run_bot() 
 
 
 if __name__ == "__main__":
